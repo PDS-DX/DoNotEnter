8d479090fbbedd23e8d96bbb34b775e76c8b371b
Preston Sparks <psparks110@gmail.com>
2025-02-19 19:18:41 -0600

    added env setup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,3 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173' # default vite port
 ]
+
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
