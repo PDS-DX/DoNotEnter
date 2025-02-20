21e289841380b20f2c272f19313068015023a71d
Preston Sparks <psparks110@gmail.com>
2025-02-19 20:47:30 -0600

    added post method override

--- backend/gptcall/views.py
+++ backend/gptcall/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .serializers import GptCallSerializer
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
 from .models import GptCall
+from .serializers import GptCallSerializer
+from .services import GPTService
+import logging
 
-# Create your views here.
-
-class GptCallView(viewsets.ModelViewSet):
-    serializer_class = GptCallSerializer
+class GptCallViewSet(ModelViewSet):
     queryset = GptCall.objects.all()
+    serializer_class = GptCallSerializer
+
+    # Overriding create (POST) method to use GPTService
+    def create(self, request, *args, **kwargs):
+        query = request.data.get("query", "")
+        if not query:
+            return Response({"error": "Query is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        gpt_service = GPTService()
+        logging.info(f"Creating GPT call with query: {query}")
+        gpt_call = gpt_service.create_gpt_call(query)
+
+        serializer = self.get_serializer(gpt_call)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
