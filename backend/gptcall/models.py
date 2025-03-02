2d68db2aaac90217211b1a187557b78dce953d12
Preston Sparks <psparks110@gmail.com>
2025-03-02 09:47:30 -0600

    #3 refactored base gpt call model to match api responses

--- backend/gptcall/models.py
+++ backend/gptcall/models.py
@@ -3,8 +3,6 @@ from django.db import models
 # Create your models here.
 
 class GptCall(models.Model):
-    query = models.TextField()
-    reply = models.TextField()
-
-    def __str__(self):
-        return self.query
+    role = models.TextField()
+    refusal = models.TextField(null=True, blank=True)
+    content = models.TextField()
