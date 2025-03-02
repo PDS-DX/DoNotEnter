3c7423b026b0e2ce36ccbec60cadd594049075af
Preston Sparks <psparks110@gmail.com>
2025-03-02 09:46:34 -0600

    #3 updated gpt call admin model

--- backend/gptcall/admin.py
+++ backend/gptcall/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import GptCall
 
 class GptCallAdmin(admin.ModelAdmin):
-    list_display = ('query', 'reply')
+    list_display = ('role', 'refusal', 'content')
 
 # Register your models here.
 
