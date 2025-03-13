b25227e9dabd8ab43891445c67833d734d6eaac3
Preston Sparks <psparks110@gmail.com>
2025-03-13 04:36:20 -0500

    #5 address todo to use env vars for api locator

--- frontend/api.ts
+++ frontend/api.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    // TODO: use env vars for host name and port
-    baseURL: 'http://localhost:8000/api',
+    baseURL: import.meta.env.VITE_API_BASE_URL,
 });
