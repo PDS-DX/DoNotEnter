2b256626156b92aa8a5e1644ca903a2ca92e9b0d
Preston Sparks <psparks110@gmail.com>
2025-03-26 20:50:59 -0500

    #7 added timer and key tracker to App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
 import "./App.css";
 import Chat from "./components/Chat.tsx";
 import { Grid2 as Grid } from "@mui/material";
+import Timer from "./components/Timer.tsx";
+import KeyTracker from "./components/KeyTracker.tsx";
 
 function App() {
     return <>
         <Grid container>
-            <Chat />
+            <Grid size={10} spacing={2}>
+                <Grid size={2} offset={5} spacing={2}>
+                    <Timer />
+                </Grid>
+                <Grid size={12} spacing={2}>
+                    <Chat />
+                </Grid>
+            </Grid>
+            <Grid size={2} spacing={2}>
+                <KeyTracker />
+            </Grid>
         </Grid>
     </>
 }
